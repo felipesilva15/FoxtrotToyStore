324ca3587704340b3f824bc1a1cfb2606ea753cb
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2023-04-11 21:22:25 -0300

    Corrigindo a centralização da página

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,8 +33,8 @@
     @stack('styles')
 </head>
 <body>
-    <header>
-        <div class="d-flex justify-content-between align-items-center px-5 py-2 bg-white shadow">
+    <header class="bg-white shadow">
+        <div class="container d-flex justify-content-between align-items-center px-5 py-2">
             <a href="{{ url('/') }}">
                 <div class="logo p-1">
                     <img src="{{ asset('images/Logo.png') }}" class="default-size" alt="Logo">
@@ -52,66 +52,64 @@
     <main>
         @yield('content')
     </main>
-        <footer class="pt-4">
-            <div class="d-flex flex-column pt-5 text-light">
-                <div class="d-flex justify-content-between flex-column flex-md-row mb-4 mx-5 gap-5">
-                    <div class="mx-1">
-                        <a href="{{ url('/') }}">
-                            <div class="logo p-1">
-                                <img src="{{ asset('images/Logo.png') }}" class="default-size" alt="Logo">
-                            </div>
+    <footer class="pt-4">
+        <div class="container d-flex flex-column pt-5 text-light">
+            <div class="d-flex justify-content-between flex-column flex-md-row mb-4 gap-5">
+                <div class="mx-1">
+                    <a href="{{ url('/') }}">
+                        <div class="logo p-1">
+                            <img src="{{ asset('images/Logo.png') }}" class="default-size" alt="Logo">
+                        </div>
+                    </a>
+                </div>
+                <div class="d-flex flex-column mx-1">
+                    <span class="fs-5 fw-bold mb-3">Atendimento ao cliente</span>
+                    <div class="d-flex align-items-center">
+                        <i class="bi bi-envelope pe-1 fs-5"></i>
+                        <a class="text-reset" href="mailto:sac@foxtrot.com">sac@foxtrot.com</a>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <i class="bi bi-telephone pe-1 fs-5"></i>
+                        <a>(11) 5555-3838</a>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <i class="bi bi-whatsapp pe-1 fs-5"></i>
+                        <a class="text-reset" href="https://api.whatsapp.com/send?phone=5511954452800">(11) 95445-2800</a>
+                    </div>
+                </div>
+                <div class="d-flex flex-column mx-1">
+                    <span class="fs-5 fw-bold mb-3">Redes sociais</span>
+                    <div class="d-flex gap-2 fs-5">
+                        <a class="text-reset" href="https://www.facebook.com/">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                        <a class="text-reset" href="https://www.instagram.com/">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                        <a class="text-reset" href="https://www.linkedin.com/">
+                            <i class="bi bi-linkedin"></i>
                         </a>
                     </div>
-                    <div class="d-flex flex-column mx-1">
-                        <span class="fs-5 fw-bold mb-3">Atendimento ao cliente</span>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-envelope pe-1 fs-5"></i>
-                            <a class="text-reset" href="mailto:sac@foxtrot.com">sac@foxtrot.com</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-telephone pe-1 fs-5"></i>
-                            <a>(11) 5555-3838</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-whatsapp pe-1 fs-5"></i>
-                            <a class="text-reset" href="https://api.whatsapp.com/send?phone=5511954452800">(11) 95445-2800</a>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-column mx-1">
-                        <span class="fs-5 fw-bold mb-3">Redes sociais</span>
-                        <div class="d-flex gap-2 fs-5">
-                            <a class="text-reset" href="https://www.facebook.com/">
-                                <i class="bi bi-facebook"></i>
-                            </a>
-                            <a class="text-reset" href="https://www.instagram.com/">
-                                <i class="bi bi-instagram"></i>
-                            </a>
-                            <a class="text-reset" href="https://www.linkedin.com/">
-                                <i class="bi bi-linkedin"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-column mx-1">
-                        <span class="fs-5 fw-bold mb-3">Métodos de pagamento</span>
-                        <div class="d-flex gap-2 fs-5">
-                            <i class="bi bi-credit-card"></i>
-                            <i class="bi bi-paypal"></i>
-                            <i class="bi bi-bank"></i>
-                            <i class="bi bi-wallet2"></i>
-                        </div>
-                    </div>
                 </div>
-                <div class="mx-5">
-                    <div class="mx-5">
-                        <hr class="default-width opacity-50">
+                <div class="d-flex flex-column mx-1">
+                    <span class="fs-5 fw-bold mb-3">Métodos de pagamento</span>
+                    <div class="d-flex gap-2 fs-5">
+                        <i class="bi bi-credit-card"></i>
+                        <i class="bi bi-paypal"></i>
+                        <i class="bi bi-bank"></i>
+                        <i class="bi bi-wallet2"></i>
                     </div>
-                </div>
-                <div class="d-flex flex-column justify-content-center align-items-center default-width">
-                    <p class="m-0 fs-5">© 2023 Foxtrot Toy Store</p>
-                    <p>Todos os direitos reservados</p>
                 </div>
             </div>
-        </footer>
+            <div class="mx-5">
+                <hr class="default-width opacity-50">
+            </div>
+            <div class="d-flex flex-column justify-content-center align-items-center default-width">
+                <p class="m-0 fs-5">© 2023 Foxtrot Toy Store</p>
+                <p>Todos os direitos reservados</p>
+            </div>
+        </div>
+    </footer>
 
     {{-- Boostrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/product', [ProductController::class, 'index'])->name('product');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
