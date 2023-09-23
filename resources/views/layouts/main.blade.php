99cc4a02f9adae8398ec8377084b9c111485ba0c
Felipe Silva <felipe.allware@gmail.com>
2023-09-23 15:32:02 -0300

    Fix: Correção do logo para que utilize o case-sensitive correto

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -43,7 +43,7 @@
         <div class="container d-flex justify-content-between align-items-center px-5 py-2">
             <a href="{{ route('home') }}">
                 <div class="logo p-1">
-                    <img src="{{ asset('images/Logo.png') }}" class="default-size" alt="Logo">
+                    <img src="{{ asset('images/logo.png') }}" class="default-size" alt="Logo">
                 </div>
             </a>
             <div id="search-bar" class="px-4 default-width">
@@ -97,7 +97,7 @@
                 <div class="mx-1">
                     <a href="{{ route('home') }}">
                         <div class="logo p-1">
-                            <img src="{{ asset('images/Logo.png') }}" class="default-size" alt="Logo">
+                            <img src="{{ asset('images/logo.png') }}" class="default-size" alt="Logo">
                         </div>
                     </a>
                 </div>
